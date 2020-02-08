e75aee028d5c4c1ae4aef7071b522f336c37c10f
Vasyl Herman <vasil.german@gmail.com>
2020-02-08 10:13:15 +0200

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
